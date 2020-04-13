0fd1ce9c558606400bdd9554f5d3cc9d4feee15c
Qianxunha <55270103+Qianxunha@users.noreply.github.com>
2020-04-13 14:09:40 +0800

    Update and rename read.txt to main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*******************************************
+time = 2019/07/13
+*******************************************/
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    cout<<"hello world"<<endl;
+    return 0;
+}
